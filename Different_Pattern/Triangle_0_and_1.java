32506ca6a38dd5ed13b54a7ff150d430da2fa473
Vivek Singh <singhvivek357480@gmail.com>
2025-04-06 22:16:58 +0530

    print 0 & 1 in trianle way!

---
+++ Different_Pattern/Triangle_0_and_1.java
@@ -0,0 +1,19 @@
+package Different_Pattern;
+
+public class Triangle_0_and_1 {
+    public static void main(String[] args) {
+        int n=9;
+
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=i; j++) {
+                int sum = i+j;
+                if(sum % 2 == 0) {
+                    System.out.print(1);
+                } else {
+                    System.out.print(0);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
