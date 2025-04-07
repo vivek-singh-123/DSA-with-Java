2dda4380767773d38fce3337e4297b922970bea3
Vivek Singh <singhvivek357480@gmail.com>
2025-04-07 22:49:29 +0530

    linear search

--- Arrays/LinearSearch.java
+++ Arrays/LinearSearch.java
@@ -1,4 +1,18 @@
 package Arrays;
 
 public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50,60,70,80,90,100};
+
+        int key =73;
+
+        for(int i=0; i<arr.length; i++) {
+            if(key==arr[i]){
+                System.out.print("Key is found: "+ key);
+                return;
+            }
+        }
+        System.out.print("Key is not found");
+
+    }
 }
