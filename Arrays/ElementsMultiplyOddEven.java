c9772bb71beadb07b899d21eef1e8852afb125c0
Vivek Singh <singhvivek357480@gmail.com>
2025-04-07 22:48:30 +0530

    elements multiply odd even

--- Arrays/ElementsMultiplyOddEven.java
+++ Arrays/ElementsMultiplyOddEven.java
@@ -1,4 +1,22 @@
 package Arrays;
 
 public class ElementsMultiplyOddEven {
+    public static void main(String[] args) {
+        int[] arr = {10,23,31,40,50,63,70,87,99,100};
+
+        for(int i=0; i< arr.length; i++) {
+            if(arr[i]%2==0){
+                System.out.print(arr[i]*2+" ");
+            }else
+            {
+                System.out.print(arr[i]*3+" ");
+            }
+        }
+    }
 }
+
+
+//sum of all the elements of an integer array
+//average of all the elements of an integer array
+//multiplication ....
+//max/min of all the elements of an integer
