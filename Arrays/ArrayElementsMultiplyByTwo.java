914fa8add825669e7b48f7f862916ab2daac3493
Vivek Singh <singhvivek357480@gmail.com>
2025-04-07 22:47:06 +0530

    array elements multiply by two

---
+++ Arrays/ArrayElementsMultiplyByTwo.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class ArrayElementsMultiplyByTwo {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50,60,70,80,90,100};
+
+        for(int i=0; i< arr.length; i++) {
+            System.out.print(2*arr[i]+" ");
+        }
+    }
+}
