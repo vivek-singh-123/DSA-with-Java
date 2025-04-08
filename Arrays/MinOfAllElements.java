f7e406401d10365b375399e2ee32ec35fdab13d9
Vivek Singh <singhvivek357480@gmail.com>
2025-04-08 13:23:27 +0530

    minimum element in the given array

--- Arrays/MinOfAllElements.java
+++ Arrays/MinOfAllElements.java
@@ -1,4 +1,16 @@
+//min of all the elements of an integer
 package Arrays;
 
 public class MinOfAllElements {
+    public static void main(String[] args) {
+        int[] arr = {46,36,59,95,66,17,75,24};
+        int minimumvalue = arr[0];
+
+        for(int i=1; i< arr.length; i++) {
+            if(arr[i]<minimumvalue){
+                minimumvalue = arr[i];
+            }
+        }
+        System.out.print("Minimum number found! and which is: "+ minimumvalue);
+    }
 }
