d4d9e551179c51d4ecd13cbd0e7dd36ec731cdb2
Vivek Singh <singhvivek357480@gmail.com>
2025-04-08 11:44:58 +0530

    average of all elements!!

--- Arrays/AverageOfAllElements.java
+++ Arrays/AverageOfAllElements.java
@@ -1,4 +1,16 @@
+//average of all the elements of an integer array
+
 package Arrays;
 
 public class AverageOfAllElements {
+    public static void main(String[] args) {
+        int[] arr = {46,36,59,95,66,21,75,24};
+        double add=0;
+
+        for(int i=0; i< arr.length; i++) {
+            add+=arr[i];
+
+        }
+        System.out.print("Average of all elements is: "+ add/arr.length);
+    }
 }
