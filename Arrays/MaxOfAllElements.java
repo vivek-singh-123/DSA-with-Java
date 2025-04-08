7a43d16adeaa5027f4e8fec7e1ffca983bdc6148
Vivek Singh <singhvivek357480@gmail.com>
2025-04-08 13:27:20 +0530

    max value in array!

--- Arrays/MaxOfAllElements.java
+++ Arrays/MaxOfAllElements.java
@@ -1,4 +1,17 @@
+//max of all the elements of an integer
 package Arrays;
 
 public class MaxOfAllElements {
+    public static void main(String[] args) {
+        int[] arr = {46,360,59,903,66,17,751,24};
+
+        int maxvalue = arr[0];
+
+        for(int i=1; i< arr.length; i++) {
+            if(arr[i]>maxvalue) {
+                maxvalue = arr[i];
+            }
+        }
+        System.out.print("max value is: "+ maxvalue);
+    }
 }
