728a1916f5c8a7893cdd79a2663871dbed05e5e7
Vivek Singh <singhvivek357480@gmail.com>
2025-04-08 12:28:07 +0530

    multiplication of all array elements

--- Arrays/MultiplicationOfAllElements.java
+++ Arrays/MultiplicationOfAllElements.java
@@ -1,4 +1,15 @@
+//multiplication of all the elements of an integer array
 package Arrays;
 
 public class MultiplicationOfAllElements {
+    public static void main(String[] args) {
+        int [] arr = {11,6,59,70,61,30};
+        int valuestore=1;
+
+        for(int i=0; i<arr.length; i++) {
+            valuestore*=arr[i];
+
+        }
+        System.out.print("Multiplication of all elements is: "+ valuestore);
+    }
 }
