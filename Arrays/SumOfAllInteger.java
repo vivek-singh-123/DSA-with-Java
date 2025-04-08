5ad89e293997150a8b67b19dc464f401c8cb2d28
Vivek Singh <singhvivek357480@gmail.com>
2025-04-08 11:13:33 +0530

    sum of all array elements

--- Arrays/SumOfAllInteger.java
+++ Arrays/SumOfAllInteger.java
@@ -1,4 +1,15 @@
+//sum of all the elements of an integer array
+
 package Arrays;
 
 public class SumOfAllInteger {
+    public static void main(String[] args) {
+        int[] arr = {37,23,31,52,18,63,70,87,99,15};
+        int sum=0;
+
+        for(int i=0; i< arr.length; i++) {
+            sum+=arr[i];
+        }
+        System.out.print("Sum of array is: "+ sum);
+    }
 }
