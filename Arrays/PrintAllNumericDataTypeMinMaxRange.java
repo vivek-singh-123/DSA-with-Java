186e9d8095bc6ebc94910cbd1b899c0676340ab6
Vivek Singh <singhvivek357480@gmail.com>
2025-04-12 10:16:14 +0530

    min & max range of all numeric data type

---
+++ Arrays/PrintAllNumericDataTypeMinMaxRange.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class PrintAllNumericDataTypeMinMaxRange {
+    public static void main(String[] args) {
+
+        System.out.println("Integer Max value is: "+Integer.MAX_VALUE+" and Min value is: "+Integer.MIN_VALUE);
+        System.out.println("Float Max value is: "+Float.MAX_VALUE+" and Min value is: "+Float.MIN_VALUE);
+        System.out.println("Double Max value is: "+Double.MAX_VALUE+" and Min value is: "+Double.MIN_VALUE);
+
+
+    }
+}
